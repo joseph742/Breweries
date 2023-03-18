a5e8b19a8fdd90da16aa103916b0432251b603de
joseph742 <umoru.j@gmail.com>
2023-03-18 12:44:23 +0100

    initial commit

---
+++ Breweries/Model/Brewery.swift
@@ -0,0 +1,14 @@
+//
+//  Brewery.swift
+//  Breweries
+//
+//  Created by umoru joseph on 2023-03-18.
+//
+
+import SwiftUI
+
+struct Brewery {
+    let name: String
+    let street: String
+    let city: String
+}
