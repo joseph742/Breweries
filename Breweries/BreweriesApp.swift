46ddc3d621f18dcd750d16806f0ce5d0faed75cb
joseph742 <umoru.j@gmail.com>
2023-03-18 12:13:51 +0100

    Initial Commit

---
+++ Breweries/BreweriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreweriesApp.swift
+//  Breweries
+//
+//  Created by umoru joseph on 2023-03-18.
+//
+
+import SwiftUI
+
+@main
+struct BreweriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreweriesUITests/BreweriesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BreweriesUITestsLaunchTests.swift
+//  BreweriesUITests
+//
+//  Created by umoru joseph on 2023-03-18.
+//
+
+import XCTest
+
+final class BreweriesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
